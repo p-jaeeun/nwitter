277e24d9578e73a85644fe5f70ac8ebe6b79e2bf
p-jaeeun <j_brick@naver.com>
2021-05-03 10:09:56 +0900

    트윗 폼 생성, 데이터베이스 연결

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,35 @@
-import React from "react";
+import { dbService } from "fBase";
+import React, { useState } from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [nweet, setNweet] = useState("");
+
+  const onHandleSubmit = async (e) => {
+    e.preventDefault();
+    await dbService.collection("nweets").add({
+      nweet,
+      createdAt: Date.now(),
+    });
+    setNweet("");
+  };
+
+  const onChange = (e) => {
+    setNweet(e.target.value);
+  };
+  return (
+    <div>
+      <form onSubmit={onHandleSubmit}>
+        <input
+          type="text"
+          value={nweet}
+          placeholder="What's in your mind?"
+          maxLength={120}
+          onChange={onChange}
+        />
+        <input type="submit" value="Ntweet" />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
