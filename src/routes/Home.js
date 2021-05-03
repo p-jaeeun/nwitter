8dd089b2d043d40287aa0b519c7976da05467a66
p-jaeeun <j_brick@naver.com>
2021-05-03 10:33:53 +0900

    트윗 작성하기, 작성된 트윗 조회하기 구현

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,23 @@
 import { dbService } from "fBase";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+
+  const getNweets = async (e) => {
+    const dbNweets = await dbService.collection("nweets").get();
+    dbNweets.forEach((document) => {
+      const nweetObj = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNweets((prev) => [nweetObj, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getNweets();
+  }, []);
 
   const onHandleSubmit = async (e) => {
     e.preventDefault();
@@ -28,6 +43,13 @@ const Home = () => {
         />
         <input type="submit" value="Ntweet" />
       </form>
+      <div>
+        {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
