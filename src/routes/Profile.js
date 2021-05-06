77df4d74f02660da4beab10478b19d0006ce1097
p-jaeeun <j_brick@naver.com>
2021-05-06 14:52:27 +0900

    프로필 변경 및 업데이트 구현

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,15 +1,56 @@
-import { authService } from "fBase";
-import React from "react";
+import { authService, dbService } from "fBase";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router";
 
-const Profile = () => {
+const Profile = ({ userObj, refreshUser }) => {
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
+
+  const getMyNweets = async () => {
+    const nweets = await dbService
+      .collection("nweets")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createdAt", "desc") // firestore에서 인덱스 추가해줘야 에러안남
+      .get();
+    console.log(nweets.docs.map((doc) => doc.data()));
+  };
+
+  useEffect(() => {
+    getMyNweets();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNewDisplayName(value);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+      refreshUser();
+    }
+  };
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Dispaly Name"
+          value={newDisplayName}
+          onChange={onChange}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
